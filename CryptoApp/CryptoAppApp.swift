bc07be31cd2b015135304939bfea346f82f78a46
Victor Chen <victor.chen@mtzendo.com>
2024-03-23 23:18:12 +1300

    add project files

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Chen Yue on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
